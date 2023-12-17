c6682825858b4871efeb13268c8e23140d240b70
Noton450 <79391702+Noton450@users.noreply.github.com>
2023-12-18 08:44:00 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ document.getElementById('login').addEventListener('submit', function(event) {
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
 
+    var errorMessage = document.getElementById('error-message');
+
     if (username === 'xelaowner' && password === 'XLpass') {
         // フォームの要素をフェードアウト
         var formElements = document.querySelector('.login-form').querySelectorAll('h2, .form-group, button');
@@ -14,7 +16,14 @@ document.getElementById('login').addEventListener('submit', function(event) {
         // チェックマークを表示（フェードアウトしない）
         var checkmarkContainer = document.getElementById('checkmark-container');
         checkmarkContainer.style.display = 'flex';
+        errorMessage.style.display = 'none';
     } else {
-        alert('ユーザー名またはパスワードが間違っています。');
+        document.getElementById('error-message').style.display = 'block';
+        errorMessage.style.display = 'block';
+        errorMessage.classList.add('blink');
+
+        errorMessage.addEventListener('animationend', function() {
+            errorMessage.classList.remove('blink');
+        });
     }
 });
